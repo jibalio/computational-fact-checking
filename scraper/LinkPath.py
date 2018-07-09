a30ee9871863a63968a2db1ed6a098adfddd49fe
Jan Leryc Villamor Ibalio <jlvibalio@addunet.ph>
2018-07-09 13:57:57 +0800

    Add LinkPath.py

---
+++ scraper/LinkPath.py
@@ -0,0 +1,16 @@
+
+"""
+Return a list of the path taken between two nodes.
+Example
+Array
+(
+    [josérizal] => José_Rizal
+    [tagaloglanguage] => Tagalog_language
+    [moon] => Moon
+)
+
+return: ["José_Rizal", "Tagalog_language", "Moon"]
+"""
+
+def get_path(a, b):
+	pass
