4839297b06c2be84c78d49d384f277644196a7c5
Student <lerycibalio@gmail.com>
2018-07-20 13:07:34 +0800

    Added new data files (countries)

--- data/clean.py
+++ data/clean.py
@@ -1,6 +1,5 @@
-o = open('ideologies_trimmed.txt','w', encoding="utf8")
+"""o = open('ideologies_trimmed.txt','w', encoding="utf8")
 with open('ideologies', 'r', encoding="utf8") as f:
-	
 	for x in f.readlines():
 		cnt = -1
 		a = x.split("; ")
@@ -9,5 +8,25 @@ with open('ideologies', 'r', encoding="utf8") as f:
 			if cnt%2==0:
 				o.write(i)
 				o.write(";")
-
 o.close()
+"""
+
+# TWO LINE SPLITTER
+x = open ("t3_x_countries.txt","w")
+y = open ("t3_y_capitals.txt", "w")
+with open('unsep', 'r', encoding="utf-8") as f:
+	cnt = -1
+	for i in f:
+		if not i.startswith("--"):
+			cnt += 1
+			if (cnt%2==0):
+				x.write(i)
+			else:
+				y.write(i)
+		else:
+			x.write(i)
+			y.write(i)
+x.close()
+y.close()
+
+
--- wikipedia_ia.py
+++ wikipedia_ia.py
@@ -14,7 +14,7 @@ import json
 import wikipedia
 import math
 
-TOKEN = r"1532025693|20fdd78accfc72489ad261e63c2b3067"
+TOKEN = r"1532061312|20fdd78accfc72489ad261e63c2b3067"
 DEBUG = True
 DEBUG_PROMPT = ">>> DEBUG:  "
 
