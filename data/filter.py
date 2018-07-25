7fc31e54e91c00ea1a1f065d88619808caa11848
ClassicLean <aleanraphael@gmail.com>
2018-07-25 14:24:13 +0800

    Created functions filterCountries, filterCapitals, and matchCapCount

--- data/filter.py
+++ data/filter.py
@@ -1,8 +1,8 @@
 import re
 
-regex = r"\w+(?=\s* [^/])"
+regex = r".*(?=\ )"
 
-countries = """
+unfilteredCountries = """
 Angola (Africa)
 Benin (Africa)
 Botswana (Africa)
@@ -10,8 +10,40 @@ Burkina Faso (Africa)
 Burundi (Africa)
 """
 
+unfilteredCapitals = """
+Luanda
+Porto-Novo
+Gaborone
+Ouagadougou
+Bujumbura
+Praia
+Bangui
+Moroni Comoros
+Kinshasa
+Brazzaville
+"""
+
 def filterCountries(countries):
     filteredCountries = re.findall(regex, countries)
-    return dict([counter + 1,country for counter, country in enumerate(filteredCountries)])
+    filteredCountries = (list(filter(None, filteredCountries)))
+    newArr = []
+    for counter, country in enumerate(filteredCountries):
+        newArr.append([counter, country])
+    return dict(newArr)
 
-print(filterCountries(countries))
+def filterCapitals(capitals):
+    filteredCapitals = capitals.split('\n')
+    newArr = []
+    for counter, capital in enumerate(filteredCapitals[1 : -1]):
+        newArr.append([counter, capital])
+    return dict(newArr)
+
+def matchCapCount():
+    filtCount = filterCountries(unfilteredCountries)
+    filtCap = filterCapitals(unfilteredCapitals)
+    result = []
+    for item in filtCount:
+        result.append([filtCount[item], filtCap[item]])
+    return result
+
+print(matchCapCount())
