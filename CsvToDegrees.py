9932691707a627c57fb2a9c7f39b34416c71f27e
Student <lerycibalio@gmail.com>
2018-08-01 14:37:41 +0800

    converttodegrees.py: can convert path list to degree list

--- CsvToDegrees.py
+++ CsvToDegrees.py
@@ -2,43 +2,77 @@
 This script takes a CSV file of paths between node A to node B.
 And converts them to degree values.
 """
-
-import csv
-with open('output/pathmatrices/t2_spouses.csv', 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=';', quotechar='|')
-    num_rows = 0
-    degrees = dict()
-    for row in csvreader:
-
-        """
-        Get the degrees of the header first
-        since it would be reused everytime.
-        Save in dictionary
-        """
-        num_rows+=1
-        if num_rows = 1:
-            y_axis = row[1:]
-            for y in y_axis:
-                if (degrees[y] != None):
-                    degrees[y] = fake_GetPath(true)
-
-        """
-        Begin parse on 2nd column.
-        """
-        
-        print (', '.join(row))
-        print("------------ROW--------------")
-
-
-
-
-
-
-
-
-def try_add():
-    pass
 def fake_GetPath(header=False):
     if header:
         return 2.52
     return 0.136
+
+
+import csv
+with open('output/pathmatrices/t2_spouses.csv', 'r') as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    current_row_idx = 0
+    degrees = dict()
+    
+    for d in csvreader:
+        
+
+        # Ignore separator column
+        if 'sep=' in d:
+            continue
+        
+        row = d[1:]
+        
+        
+        
+        
+        entities = [x.split('>') for x in row]
+
+        """
+        # run this if header row.
+        current_row_idx += 1
+        if current_row_idx == 1:
+            for x in entities:
+                if x[
+                print(x[0])
+        """
+
+        # SKIP FIRST ROW
+        current_row_idx += 1
+        if current_row_idx == 1:
+            continue
+
+        degree_row_x = []
+        for path in entities:
+            
+            for entity in path:
+
+                ## if entity is not recorded in dictionary
+                ## try to calculate before accessing
+                if entity not in degrees:
+                    degrees[entity] = fake_GetPath()
+
+                ## in this part, degree of entity is guaranteed
+                ## to exist
+
+            pathdeg = ([str(degrees[a]) for a in path])
+            pathdeg = ">".join(pathdeg)
+            degree_row_x.append(pathdeg)
+
+        print(degree_row_x)
+        print("%s\n----------------------------" % entities)
+
+
+
+
+
+
+
+
+
+def try_add(key):
+    if key not in degrees:
+        degrees[key] = fake_GetPath()
+        
+
