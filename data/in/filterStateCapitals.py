e29d197102aead5863f6a6e2060848e979cf9630
ClassicLean <aleanraphael@gmail.com>
2018-09-10 15:33:18 +0800

    Added filterOscars.py

---
+++ data/in/filterStateCapitals.py
@@ -0,0 +1,55 @@
+midwest = '''
+Springfield	Illinois
+Indianapolis	Indiana
+Des Moines	Iowa
+Topeka	Kansas
+Lansing	Michigan
+Saint Paul	Minnesota
+Jefferson City	Missouri
+Lincoln	Nebraska
+Bismarck	North Dakota
+Columbus	Ohio
+Pierre	South Dakota
+Madison	Wisconsin
+'''
+
+northeast = '''
+Hartford	Connecticut
+Augusta Boston	Maine
+Concord	Massachusetts
+Albany	New York
+Harrisburg	Pennsylvania
+Providence	Rhode Island
+Montpelier	Vermont
+'''
+
+south = '''
+Montgomery	Alabama
+Little Rock	Arkansas
+Dover	Delaware
+Tallahassee	Florida
+Atlanta	Georgia U S state
+Frankfort	Kentucky
+Baton Rouge	Louisiana
+Annapolis	Maryland
+Jackson	Mississippi
+Raleigh	North Carolina
+Oklahoma City	Oklahoma
+Columbia	South Carolina
+Nashville	Tennessee
+Austin	Texas
+Richmond	Virginia
+Charleston	West Virginia
+'''
+
+west = '''
+Juneau	Alaska
+Phoenix	Arizona
+Sacramento	California
+Honolulu Boise	Hawaii
+Carson City	Nevada
+Santa Fe	New Mexico
+Salem	Oregon
+Salt Lake City	Utah
+Cheyenne	Wyoming
+'''
